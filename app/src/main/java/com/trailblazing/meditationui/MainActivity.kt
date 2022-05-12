7058e181d4891078ee51d2c2b6485e4c011b1307
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-05-12 19:19:17 +0530

    initial commit

---
+++ app/src/main/java/com/trailblazing/meditationui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.trailblazing.meditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.trailblazing.meditationui.ui.theme.MeditationUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUITheme {
+
+            }
+        }
+    }
+}
