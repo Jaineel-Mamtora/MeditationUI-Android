258ef4eb873aff68cbf54cae27d0086437b1972a
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-05-13 16:05:47 +0530

    added: Meditation UI

--- app/src/main/java/com/trailblazing/meditationui/MainActivity.kt
+++ app/src/main/java/com/trailblazing/meditationui/MainActivity.kt
@@ -3,14 +3,17 @@ package com.trailblazing.meditationui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import com.trailblazing.meditationui.ui.HomeScreen
 import com.trailblazing.meditationui.ui.theme.MeditationUITheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUITheme {
-
+                HomeScreen()
             }
         }
     }
